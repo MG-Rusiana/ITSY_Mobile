6b9c62dfa7653a91fdfc15e49b4bdcc8a0db8c4f
Mark Gil P. Rusiana <markgilrusiana437@gmail.com>
2023-10-02 17:20:40 +0800

    feat: changes in addin items

--- src/screens/dashboard.js
+++ src/screens/dashboard.js
@@ -1,8 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { View, Text, StatusBar, Image, TouchableOpacity, ScrollView  } from 'react-native';
-
-import AddIcon from '@mui/icons-material/Add';
-import FileUploadOutlinedIcon from '@mui/icons-material/FileUploadOutlined';
 
 import { NavBar } from '../components/navBar'
 import { InputFields } from '../components/input';
@@ -11,6 +8,64 @@ import { CheckBox } from '../components/checkBox';
 
 export const Dashboard = ({navigation}) => {
 
+    const [items,setItems] = useState({
+        name:"", 
+        qk:"",
+    })
+    const [product,setproducts]= useState([])
+    const [messages, setMessages] = useState([]);
+
+    const handleNameChange = (text) => {
+        setItems({ ...items, name: text });
+      };
+    
+    const handleQkChange = (text) => {
+        setItems({ ...items, qk: text });
+    };
+
+
+    function addItems() {
+
+        if (items.name !="") {
+          setproducts([...product,`${items.qk} ${items.name}`])
+        
+        const newMessage = {
+          product: product.length ==0? `${items.qk} ${items.name}`:`${[...product,` ${items.qk} ${items.name}`]} ` ,
+          direction: 'outgoing',
+          from: "user"
+        };
+        
+    
+        setMessages([...messages, newMessage]);
+    
+    
+        console.table(messages)
+        setItems({name:"",qk:""})
+        }
+    
+      }
+
+      //Delete Items
+      const handleDeleteItem = (index) => {
+        // Create a new array that excludes the item at the given index
+        const updatedProduct = product.filter((_, i) => i !== index);
+        setproducts(updatedProduct);
+      };
+
+      const scrollViewRef = useRef(null);
+
+      // Function to scroll to the bottom of the ScrollView
+      const scrollToBottom = () => {
+        if (scrollViewRef.current) {
+          scrollViewRef.current.scrollToEnd({ animated: true });
+        }
+      };
+    
+      useEffect(() => {
+        scrollToBottom();
+      }, [messages]);
+
+   
 
     const [foodPref, setFoodPref] = useState(false)
 
@@ -37,11 +92,11 @@ export const Dashboard = ({navigation}) => {
     
 
 
-    const addItem = () => {
-        console.log("Add Item button clicked!")
-    }
+    // const addItem = () => {
+    //     console.log("Add Item button clicked!")
+    // }
 
-    const items = [
+    const sc_item = [
         {name: "Egg", quantity_weight: 9},
         {name: "Egg", quantity_weight: 9},
         {name: "Egg", quantity_weight: 9},
@@ -63,6 +118,8 @@ export const Dashboard = ({navigation}) => {
         console.log("Select Food Pref: ", foodPref)
         console.log("Select Image to scan: ", selectImage)
         console.log("Scan Image: ", isScanned)
+        console.log(items)
+        console.log(product)
         console.log("===============================================================")
     })
 
@@ -87,7 +144,7 @@ export const Dashboard = ({navigation}) => {
 
 
             {/* Inside the green box */}
-            {isScanned ? 
+            {/* {isScanned ? 
                 <ScrollView className=" w-full h-[63%] flex flex-col border-[1px] border-[#1EDF38] rounded-md p-7 overflow-hidden">
 
                     <View className=" w-full items-end mb-5 "> 
@@ -98,13 +155,13 @@ export const Dashboard = ({navigation}) => {
                         <Text className=" text-[#ffffff] text-[20px] font-bold">Items</Text>
                     </View>
 
-                    {items.map((items, index) => (
+                    {sc_item.map((sc_item, index) => (
                         <View className=" w-full h-[40px] flex flex-row">
                             <View className=" w-[70%] h-full pl-4">
-                                <Text className=" text-[17px] ">{items.name}</Text>
+                                <Text className=" text-[17px] ">{sc_item.name}</Text>
                             </View>
                             <View className=" w-[30%] h-full pl-4">
-                                <Text className=" text-[17px]">{items.quantity_weight}</Text> 
+                                <Text className=" text-[17px]">{sc_item.quantity_weight}</Text> 
                             </View>                       
                         </View>
                     ))}
@@ -121,8 +178,102 @@ export const Dashboard = ({navigation}) => {
                 <Text className=" text-[#AFAFAF] text-[20px] w-[80%] text-center leading-8 mt-4">Hey there! I'm ITSY, your culinary spider-body! Share your items, I'll weave menus so snappy!</Text>
             
             </View>    
-            }
+            } */}
+            <View className = "relative w-full h-[63%] overflow-hidden border-[1px] border-[#1EDF38]">
+            <ScrollView
+                    ref={scrollViewRef}
+                    onContentSizeChange={scrollToBottom}
+                    className=" flex-col rounded-md pb-[100px] p-7 "
+                >
+                    {messages.length !== 0 ? (
+                    messages.map((e, id) =>
+                        e.from === 'user' ? (
+                        <View
+                            className = " w-[100%] min-h[15px] bg-[#ffffff] flex flex-row justify-end pt-[3px] pb-[20px]"
+                            key={id}
+                        >
+                            <View
+                                className = " px-[5px] py-[2px] bg-[#1EDF38] rounded-lg "
+                            >
+                            {/* {e.image ? (
+                                <>
+                                <Text style={{ paddingTop: 1, paddingBottom: 4, color: 'white' }}>{e.message}</Text>
+                                <Image
+                                    style={{ width: '100%', height: 56, aspectRatio: 1, borderRadius: 10 }}
+                                    source={{ uri: e.image }}
+                                />
+                                </>
+                            ) : (
+                                ''
+                            )} */}
 
+                            {e.product 
+                                ? 
+                                <View className = " w-full pb-2 px-2">
+                                <View className="  bg-[#1EDF38] h-[40px] rounded-md justify-center">
+                                    <Text className=" text-[#ffffff] text-[20px] font-bold text-center">My Items</Text>
+                                </View>
+                                {product.map((item, index) => (
+                                    <View className=" w-full rounded-md flex items-center flex-row bg-[#ffffff] mt-2 px-2" key={index}>
+                                        
+                                        <TouchableOpacity
+                                            onPress={() => handleDeleteItem(index)}>
+                                            <Image 
+                                                source={require('./../images/delete.png')}
+                                                style={{ width: 30, height: 45 }}
+                                                resizeMode="contain"
+                                            />
+                                        </TouchableOpacity>
+
+                                        <Text className=" text-[17px] text-[#808080] font-bold m-2">{item}</Text> 
+
+                                    </View>
+                                ))}
+                                </View>                             
+                                : ''
+                            }                        
+
+                            </View>
+                        </View>
+                        ) : (
+                        <View
+                            key={id}
+                            className = " w-full min-h-[150px] border-b-[#1EDF38] border-b-[1px] bg-[#ffffff] flex flex-row items-center justify-start"
+                        >
+                            <View
+                            className = " px-2 max-w-60 py-3 pr-8 ml-3 rounded-10 border-[1px] border-[#000000] shadow-md flex flex-row items-center justify-between "
+                            >
+                            <Image source={{ uri: logo }} style={{ width: 45, height: 45 }} />
+                            <View>
+                                <Text style={{ fontSize: 20, fontWeight: 'bold', color: 'green' }}>ITSY</Text>
+                                {e.message ? <Text style={{ fontSize: 14, fontWeight: 'bold', color: 'black' }}>{e.message}</Text> : ''}
+                            </View>
+                            </View>
+
+                            {e.image ? <Image style={{ width: 56, height: 52 }} source={{ uri: e.image }} /> : ''}
+                        </View>
+                        
+                        )   
+                    )
+                    ) : (
+                    <View
+                        className = " flex flex-col items-center justify-center py-[20px]"
+                    >
+                        <Image 
+                            source={require('./../images/logo2.png')} 
+                            style={{ width: 60, height: 70 }} 
+                            resizeMode='contain'
+                        />
+
+                        <Text 
+                            className = " text-[18px] text-[#808080] text-center font-semibol leading-8 ">
+                            Hey dear, I'm ITSY your culinary spider buddy! Share your items, and I'll weave dishes so snappy!
+                        </Text>
+                    </View>
+                    )}
+                    <View className= " h-[50px] "></View>
+                </ScrollView>
+                </View>
 
 
             {/* Select preference and Upload Image buttons */}
@@ -155,12 +306,15 @@ export const Dashboard = ({navigation}) => {
                 <InputFields 
                     label="Name"
                     placeholder="e.g egg, chicken"
+                    onChangeText = {handleNameChange}
+                    value = {items.name}
                 />
 
                 <InputFields 
                     label="Quantity/Weight(kg)"
-                    placeholder="e.g 2, 5, 4"
-                    keyboardType = "numeric"
+                    placeholder="e.g 2, 5, 4kg"
+                    onChangeText = {handleQkChange}
+                    value = {items.qk}
                 />
 
             </View>
@@ -171,7 +325,7 @@ export const Dashboard = ({navigation}) => {
             {/* Add item button */}
             <View className=" w-full mt-4">
                 <Buttons 
-                    onPress = {addItem}
+                    onPress = {addItems}
                     label = "Add Item"
                     addStyle = " w-[130px] h-[40px] bg-[#1EDF38] "
                 />
